908dc21fa0cf063629270842b3c1d7fe3bc12e88
AmielZuniga <ajzs.093@gmail.com>
2014-09-20 14:09:47 -0500

    Changes
    2nd commit

--- shell4.c
+++ shell4.c
@@ -9,15 +9,15 @@
 int
 main(void)
 {
-    char    buf[MAXLINE];    
+    char    buf[MAXLINE];
     pid_t    pid;
     int        status;
-
-    printf("%% ");    
+    // Change
+    printf("%% ");
     fgets(buf, MAXLINE, stdin);
     if (buf[strlen(buf) - 1] == '\n')
-        buf[strlen(buf) - 1] = 0; 
+        buf[strlen(buf) - 1] = 0;
     execlp("sh", "sh", "-c", buf, (char *)0);
     printf("couldn't execute: %s", buf);
-    exit(127);    
+    exit(127);
 }
