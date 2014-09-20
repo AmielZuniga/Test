a1aa804ce1765af83efb624fc3dfb9e54dffaf93
AmielZuniga <ajzs.093@gmail.com>
2014-09-20 14:07:25 -0500

    C code example
    C code example.

---
+++ shell4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define  MAXLINE 255
+
+int
+main(void)
+{
+    char    buf[MAXLINE];    
+    pid_t    pid;
+    int        status;
+
+    printf("%% ");    
+    fgets(buf, MAXLINE, stdin);
+    if (buf[strlen(buf) - 1] == '\n')
+        buf[strlen(buf) - 1] = 0; 
+    execlp("sh", "sh", "-c", buf, (char *)0);
+    printf("couldn't execute: %s", buf);
+    exit(127);    
+}
