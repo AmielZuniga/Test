a5b9f117a1f27115db8453e9c9255efd49f1fb44
Marysolsnz <marysol2295@gmail.com>
2014-09-21 23:46:36 -0500

    Ejemplo muy básico en Unity
    Mover la cámara.

---
+++ Demo/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //Para usar list;
+
+public class HelloWorld : MonoBehaviour {
+
+	public List<string> list = new List<string>(); //Los atributos publicos se pueden modificar desde el inspector del objeto.
+	public float speed = 2f;
+	// Use this for initialization
+	void Start () {
+		print (list [0] + list [1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform es el de el inspector del objeto
+		transform.Translate(new Vector3(speed, 0, transform.position.z)* Time.deltaTime);
+	}
+}
